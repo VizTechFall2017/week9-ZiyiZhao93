64f213545904f201a742b747aae5cffc94d593c4
ZiyiZhao93 <31828737+ZiyiZhao93@users.noreply.github.com>
2017-11-15 00:06:57 -0500

    11.15

--- PS01/new.js
+++ PS01/new.js
@@ -1,8 +1,19 @@
-var width = document.getElementById('svg1','svg2','svg3').clientWidth;
-var height = document.getElementById('svg1','svg2','svg3').clientHeight;
+var width = document.getElementById('svg1').clientWidth;
+var height = document.getElementById('svg1').clientHeight;
+
+var width2 = document.getElementById('svg2').clientWidth;
+var height2 = document.getElementById('svg2').clientHeight;
+
+var width3 = document.getElementById('svg3').clientWidth;
+var height3 = document.getElementById('svg3').clientHeight;
+
+console.log(width,width2,width3);
 
 var marginLeft = 0;
 var marginTop = 0;
+
+var marginLeft3 = 100;
+var marginTop3 = 100;
 
 var nestedData = [];
 
@@ -16,7 +27,7 @@ var svg2 = d3.select('#svg2')
 
 var svg3 = d3.select('#svg3')
     .append('g')
-    .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
+    .attr('transform', 'translate(' + marginLeft3 + ',' + marginTop3 + ')');
 
 
 var albersProjection = d3.geoAlbers()
@@ -33,14 +44,15 @@ var cityLookup = d3.map();
 
 var colorScale = d3.scaleLinear().range(['orange','orange']);
 
-var scaleX = d3.scaleBand().rangeRound([0, width-2*marginLeft]).padding(0.5);
-var scaleY = d3.scaleLinear().range([height-2*marginTop, 0]);
+var scaleX = d3.scaleBand().rangeRound([0, width3-2*marginLeft3]).padding(0.5);
+var scaleY = d3.scaleLinear().range([height3-2*marginTop3, 0]);
 
 
 queue()
     .defer(d3.json, "./boston1.json")
+    .defer(d3.json, "./subway.json")
     .defer(d3.csv, "./subway.csv")
-    .await(function(err, mapData, populationData){
+    .await(function(err, mapData,lineData, populationData){
 
 
         populationData.forEach(function(d){
@@ -68,56 +80,42 @@ queue()
                 d3.select(this).attr('fill', function (d) {
                     return colorScale(cityLookup.get(d.properties.NAME));
                 })
-            })
-
-    });
-
-
-queue()
-    .defer(d3.json, "./subway.json")
-    .defer(d3.csv, "./subway.csv")
-    .await(function(err, mapData, populationData) {
-
-
+            });
 
         svg2.selectAll("path")               //make empty selection
-            .data(mapData.features)          //bind to the features array in the map data
+            .data(lineData.features)          //bind to the features array in the map data
             .enter()
             .append("path")                 //add the paths to the DOM
             .attr("d", path)                //actually draw them
             .attr("class", "feature")
-            .attr('stroke', 'black')
-            .attr('stroke-width', .2);
+            .attr('stroke', 'orange')
+            .attr('stroke-width', .5)
+            .attr('fill','none');
+
+        nestedData = d3.nest()
+            .key(function(d){return d.gender})
+            .entries(populationData);
+
+        console.log(nestedData);
+
+        var loadData = nestedData.filter(function(d){return d.key == 'total'})[0].values;
+
+
+        svg3.append("g")
+            .attr('class','xaxis')
+            .attr('transform','translate(0,'+ (height3-2*marginTop3) +')')
+            .call(d3.axisBottom(scaleX));
+
+        svg3.append("g")
+            .attr('class', 'yaxis')
+            .call(d3.axisLeft(scaleY));
+
+        drawPoints(loadData,'total');
 
     });
 
 
-
-
-
-
-d3.csv('./subway.csv', function(dataIn) {
-
-    nestedData = d3.nest()
-        .key(function(d){return d.gender})
-        .entries(dataIn);
-
-    var loadData = nestedData.filter(function(d){return d.key == 'total'})[0].values;
-
-    svg.append("g")
-        .attr('class','xaxis')
-        .attr('transform','translate(0,'+ (height-2*marginTop) +')')
-        .call(d3.axisBottom(scaleX));
-
-    svg.append("g")
-        .attr('class', 'yaxis')
-        .call(d3.axisLeft(scaleY));
-
-    drawPoints(loadData);
-
-});
-
-function drawPoints(pointData){
+function drawPoints(pointData,gender){
 
 
     scaleX.domain(pointData.map(function(d){return d.things;}));
@@ -131,7 +129,7 @@ function drawPoints(pointData){
         .call(d3.axisLeft(scaleY));
 
 
-    var rects = svg.selectAll('.bars')
+    var rects = svg3.selectAll('.bars')
         .data(pointData, function(d){return d.things;});
 
     console.log(pointData);
@@ -149,14 +147,25 @@ function drawPoints(pointData){
             return scaleX.bandwidth();
         })
         .attr('height',function(d){
-            return height-2*marginTop - scaleY(+d.number);
+            return height3-2*marginTop3 - scaleY(+d.number);
+        })
+        .attr('fill',function(d){
+            if (gender == 'total'){return "mediumslateblue"}
+            if (gender == 'male'){return "lightblue"}
+            if (gender == 'female'){return "pink"}
         });
+
+    console.log(gender);
 
     rects
         .enter()
         .append('rect')
         .attr('class','bars')
-        .attr('fill', "mediumslateblue")
+        .attr('fill',function(d){
+            if (gender == 'total'){return "mediumslateblue"}
+            if (gender == 'male'){return "lightblue"}
+            if (gender == 'female'){return "pink"}
+        })
         .attr('id',function (d) {return d.things})
         .attr('x',function(d){
             return scaleX(d.things);
@@ -168,20 +177,36 @@ function drawPoints(pointData){
             return scaleX.bandwidth();
         })
         .attr('height',function(d){
-            return height-2*marginTop - scaleY(+d.number);
+            return height3-2*marginTop3 - scaleY(+d.number);
         })
 
 }
 
 function updateData(selectedGender){
-    return nestedData.filter(function(d){return d.key == 'total'})[0].values;
+
+
+
+    return nestedData.filter(function(d){return d.key == selectedGender})[0].values;
 }
 
 
 function sliderMoved(value){
 
-    newData = updateData(value);
-    drawPoints(newData);
+    if(value == 0){
+        newData = updateData('total');
+        drawPoints(newData,'total');
+    }
+
+    if(value == 1){
+        newData = updateData('male');
+        drawPoints(newData,'male');
+    }
+
+    if(value == 2){
+        newData = updateData('female');
+        drawPoints(newData,'female');
+    }
+
 
 }
 
